4b7f3ac662df57681da2d6642a7e573b0c3557d0
Dekard89 <nasenkov_89@mail.ru>
2023-05-03 13:41:48 +0300

    тест

--- Program.cs
+++ Program.cs
@@ -2,58 +2,35 @@
 using Telegram.Bot.Types;
 using System.Linq;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Polling;
 
-namespace Telegrambot
+class Program
 {
-    internal class Program
+    private static TelegramBotClient botClient;
+    private static string Token = "6048952678:AAHcbgbMFMU0WS3Umma37SoM3TAu--Wt-K8";
+
+    ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+{
+    new KeyboardButton[] { "База Данных", "Калькулятор" },
+});
+
+    static void Main(string[] args)
     {
-        static void Main()
-        {
-            var botClient = new TelegramBotClient("6048952678:AAHcbgbMFMU0WS3Umma37SoM3TAu--Wt-K8");
-            botClient.StartReceiving(Update, Error);
-            
-            Console.ReadLine();
-            
+        botClient=new TelegramBotClient(Token);
+        botClient.GetUpdatesAsync().Wait(1000);
+        Console.ReadLine();
+        botClient.StartReceiving(Updater, Erorr);
+    }
 
-            string path = @"File.doc";
+    private static Task Erorr(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+    {
+        throw new NotImplementedException();
+    }
 
-
-
-        }
-
-        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
-        {
-            throw new NotImplementedException();
-        }
-
-        async private static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
-        {
-            var massege = update.Message;
-
-            if (massege != null)
-            {
-
-                if (massege.Text.ToLower().Contains("здарова"))
-                    await botClient.SendTextMessageAsync(massege.Chat.Id, "ну здорова, введи команду");
-                if (massege.Text.ToLower().Contains("привет"))
-                    await botClient.SendTextMessageAsync(massege.Chat.Id, "ну привет,введи команду");
-                if (massege.Text.ToLower().Contains("поиск"))
-                    await botClient.SendTextMessageAsync(massege.Chat.Id, "что ищем");
-                var command = massege.Text.ToLower();
-                string path = @"File.doc";
-                using (var streamRider = new StreamReader(path))
-                   while
-
-
-
-
-
-            }
-
-
-        }
-
-       
+    private static Task Updater(ITelegramBotClient arg1, Update arg2, CancellationToken arg3)
+    {
+        throw new NotImplementedException();
     }
 }
 
