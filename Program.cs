22ea106a000787a75822ebd7cef15796a0bbfb83
Dekard <130221190+Dekard89@users.noreply.github.com>
2023-05-03 14:32:35 +0300

    testAdd files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,71 @@ using System.Linq;
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Exceptions;
 
 class Program
 {
     private static TelegramBotClient botClient;
     private static string Token = "6048952678:AAHcbgbMFMU0WS3Umma37SoM3TAu--Wt-K8";
 
-    ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
-{
-    new KeyboardButton[] { "База Данных", "Калькулятор" },
-});
+    private static  ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+  {
+    new KeyboardButton[] { "поиск", "Калькулятор" },
+})
+    {
+        ResizeKeyboard = true
+    };
 
     static void Main(string[] args)
     {
-        botClient=new TelegramBotClient(Token);
-        var me = botClient.GetMeAsync().Result;
-        botClient.GetUpdatesAsync().Wait(1000);
-        Console.ReadLine();
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>()
+        };
+
+        botClient = new TelegramBotClient(Token);
+       
+        var me =  botClient.GetMeAsync();
+
+        
+ 
         botClient.StartReceiving(Updater, Erorr);
+        Console.ReadLine(); 
     }
 
-    private static Task Erorr(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+    private  static Task Erorr(ITelegramBotClient arg1, Exception exception, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+
     }
 
-    private async static Task Updater(ITelegramBotClient botClient, Update update, CancellationToken arg3)
+    private async static Task Updater(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        var messege = update.Message;
+        var msg = update.Message;
+
+        if (msg != null)
+        {
+            if (msg.Text.ToLower().Contains("привет") || msg.Text.ToLower().Contains("здорова"))
+            {
+                await botClient.SendTextMessageAsync(msg.Chat.Id, $"ну, {msg.Text},начнём?", replyMarkup:replyKeyboardMarkup);
+                
+
+            }
+
+            return;
+
+        }
+
+
     }
+
 }
 
